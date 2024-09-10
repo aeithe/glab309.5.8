4a32ffdf2685a104eccd3333708c67bd9a8377f9
Arisa <pisareva.alisa@gmail.com>
2024-09-10 13:10:03 -0400

    first commit

---
+++ src/main/java/com/example/glab30958/DemoController.java
@@ -0,0 +1,10 @@
+package com.example.glab30958;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoController {
+    @GetMapping("/get-info")
+    public String getStringMessage() {
+        return "Hi ! You will be able to monitor the health of the application !!" ;
+    }
+}
